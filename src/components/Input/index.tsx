96dd0588dd38f86dc8ed8e9c2c28eecc7774d7cb
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-09-17 09:30:24 -0300

    Refactor ModalAddFood - Typescript

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState, useCallback, InputHTMLAttributes } from 'react'
+import { useEffect, useRef, useState, useCallback } from 'react'
 
 import { useField } from '@unform/core'
 
